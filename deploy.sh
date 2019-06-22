29d6ae8b63eb34bfa7f5ab6db7273cfddf7eccce
Rasmus Toivanen <rasmus.toivanen@hotmail.com>
2019-06-22 19:25:20 +0300

    aadded deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+docker build -t rasmust92/multi-client:latest -t rasmust92/multi_client:$SHA -f ./client/Dockerfile ./client
+docker build -t rasmust92/multi-server:latest -t rasmust92/multi_server:$SHA -f ./server/Dockerfile ./server
+docker build -t rasmust92/multi-worker:latest -t rasmust92/multi_worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push rasmust92/multi-client:latest
+docker push rasmust92/multi-server:latest
+docker push rasmust92/multi-worker:latest
+
+docker push rasmust92/multi-client:$SHA
+docker push rasmust92/multi-server:$SHA
+docker push rasmust92/multi-worker:$SHA
+
+
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=rasmust92/multi-server:$SHA
+kubectl set image deployments/server-deployment client=rasmust92/multi-client:$SHA
+kubectl set image deployments/server-deployment worker=rasmust92/multi-worker:$SHA
